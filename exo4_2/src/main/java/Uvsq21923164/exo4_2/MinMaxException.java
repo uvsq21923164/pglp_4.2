8438d664f9bcbe4aaa66888acae962cb0e7564f2
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-30 14:18:16 +0200

    creation classe MinMaxException

---
+++ exo4_2/src/main/java/Uvsq21923164/exo4_2/MinMaxException.java
@@ -0,0 +1,9 @@
+package Uvsq21923164.exo4_2;
+
+public class MinMaxException extends ArithmeticException {
+
+	
+	public MinMaxException() {
+		super("Valeur absolue trop grande pour être gérée");
+	}
+}
