de4ba6a9df189f28b8f31604c9767f864ddea8b8
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-30 13:54:27 +0200

    énumérer les operations

---
+++ exo4_2/src/main/java/Uvsq21923164/exo4_2/Operation.java
@@ -0,0 +1,62 @@
+package Uvsq21923164.exo4_2;
+
+import Uvsq21923164.exo4_2.exc.DivisionParZeroException;
+
+
+public enum Operation {
+	PLUS("+"){
+
+		
+		public double eval(double x, double y) {
+			return x + y;
+		}
+
+	}, 
+
+	MOINS("-"){
+
+		@Override
+		public double eval(double x, double y) {
+			return x - y;
+		}
+
+	},
+
+	MULT("*"){
+
+		
+		public double eval(double x, double y) {
+			return x * y;
+		}
+
+	},
+
+	DIV("/"){
+
+		
+		public double eval(double x, double y) {
+			if(y == 0) {
+				throw new DivisionParZeroException();
+			}
+			return x / y;
+		}
+
+	};
+
+	private String symbole;
+
+	
+	private Operation(String symbole) {
+		this.symbole = symbole; 
+	}
+
+	
+	public abstract double eval(double x , double y);
+
+	
+	public String getSymbole() {
+		return symbole;
+	}
+
+
+}
