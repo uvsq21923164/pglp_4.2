3d149fe7b3a3541d7b6b06d5cf713cd5e9094db9
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-30 14:16:41 +0200

    creation exceptionhandler

---
+++ exo4_2/src/main/java/Uvsq21923164/exo4_2/ExceptionHandler.java
@@ -0,0 +1,20 @@
+package Uvsq21923164.exo4_2;
+
+public class ExceptionHandler {
+	
+	private static final double MIN_VALUE = 100000;
+	private static final double MAX_VALUE = 100000;
+	
+	public static void handleMinMax(double nb) {
+		if(nb > MAX_VALUE || nb < -MIN_VALUE) {
+			throw new MinMaxException();
+		}
+	}
+	
+	public static void handleStackSize(int actualSize, int wantedSize) {
+		if(actualSize < wantedSize) {
+			throw new ArgumentPileException();
+		}
+	}
+
+}
