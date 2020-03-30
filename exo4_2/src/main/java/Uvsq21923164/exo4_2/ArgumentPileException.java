5fe43ef551d0d6aea63b9bc313e0bac6e4c01c98
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-30 14:12:11 +0200

    creation classe ArgumentPileException

---
+++ exo4_2/src/main/java/Uvsq21923164/exo4_2/ArgumentPileException.java
@@ -0,0 +1,11 @@
+package Uvsq21923164.exo4_2;
+
+
+public class ArgumentPileException extends IllegalArgumentException {
+
+	
+	public ArgumentPileException() {
+		super("Pas assez d'éléments dans la pile pour cette opération");
+	}
+
+}
