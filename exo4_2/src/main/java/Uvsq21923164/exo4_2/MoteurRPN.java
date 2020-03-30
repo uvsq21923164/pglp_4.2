aa61631fdf33719db62107d4f9981d43d16bf489
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-30 14:24:20 +0200

    creation classe moteurrpn avec des methodes enregistre,appliqueroperation,affichepile,return taille pile

---
+++ exo4_2/src/main/java/Uvsq21923164/exo4_2/MoteurRPN.java
@@ -0,0 +1,40 @@
+package Uvsq21923164.exo4_2;
+import java.util.Stack;
+
+import Uvsq21923164.exo4_2.cmd.OperationCommand;
+import Uvsq21923164.exo4_2.cmd.StoreNbCommand;
+
+public class MoteurRPN extends Interpreteur {
+
+	private Stack<Double> pile;
+
+	
+	public MoteurRPN() {
+		this.pile = new Stack<Double>();
+	}
+
+	public void enregistrerNb(double nb) {
+		this.applyStoreCommand(new StoreNbCommand(pile, nb));
+	}
+
+	
+	public void appliquerOperation(Operation op) {
+		this.applyStoreCommand(new OperationCommand(pile, op));
+	}
+
+	
+
+	public String afficherPile() {
+		String s = "[";
+		for (Double d : this.pile) {
+			s += (d + " ");
+		}	
+		return s.trim() + "]";
+	}
+
+	
+
+	public int getSizePile(){
+		return this.pile.size();
+	}
+}
