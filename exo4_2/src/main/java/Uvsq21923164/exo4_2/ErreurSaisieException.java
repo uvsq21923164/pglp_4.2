0650e551f94cb6f041a23e76e8fb2a4a76c9c1cf
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-30 16:36:47 +0200

    classe ErreurSaisieException

---
+++ exo4_2/src/main/java/Uvsq21923164/exo4_2/ErreurSaisieException.java
@@ -0,0 +1,13 @@
+package Uvsq21923164.exo4_2;
+
+
+
+
+public class ErreurSaisieException extends IllegalArgumentException {
+
+	
+	public ErreurSaisieException() {
+		super("Erreur de saisie.");
+	}
+
+}
