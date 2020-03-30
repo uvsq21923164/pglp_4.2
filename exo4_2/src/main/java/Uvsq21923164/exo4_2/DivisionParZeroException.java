7d4cd12c24e5ef14d5e8d87f2ed112e754db6fcf
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-30 14:13:59 +0200

    creation classe DivisionParZeroException

---
+++ exo4_2/src/main/java/Uvsq21923164/exo4_2/DivisionParZeroException.java
@@ -0,0 +1,10 @@
+package Uvsq21923164.exo4_2;
+
+
+public class DivisionParZeroException extends ArithmeticException {
+
+	public DivisionParZeroException() {
+		super("Division par zéro");
+	}
+
+}
